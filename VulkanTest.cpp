d5502126ca99beccae1f14418a59acdb79f4bd54
thrawn235 <thrawn235@gmail.com>
2016-09-26 17:47:43 +0200

    blabla

--- VulkanTest.cpp
+++ VulkanTest.cpp
@@ -58,6 +58,8 @@ class VulkanEngine
 	VkPipelineLayout pipelineLayout;
 	VkPipeline graphicsPipeline;
 	vector<VkFramebuffer> swapChainFramebuffers;
+	VkCommandPool commandPool;
+	vector<VkCommandBuffer> commandBuffers;
 	
 	public:
 	void PollEvents()
@@ -628,6 +630,66 @@ class VulkanEngine
 		}
 		cout<<"Done creating Framebuffers!"<<endl<<endl;
 	}
+	void CreateCommandPool()
+	{
+		cout<<"creating command pool..."<<endl;
+		VkCommandPoolCreateInfo poolInfo = {};
+		poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+		poolInfo.queueFamilyIndex = queueFamilyIndex;
+		poolInfo.flags = 0;
+		
+		cout<<"calling vkCreakeCommandPool..."<<endl;
+		vkCreateCommandPool(device, &poolInfo, NULL, &commandPool);
+		cout<<"done creating command pool!"<<endl<<endl;
+	}
+	void CreateCommandBuffers()
+	{
+		cout<<"creating CommandBuffers..."<<endl;
+		commandBuffers.resize(swapChainFramebuffers.size());
+		
+		VkCommandBufferAllocateInfo allocInfo = {};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.commandPool = commandPool;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandBufferCount = commandBuffers.size();
+		
+		cout<<"calling vkAllocateCommandBuffers..."<<endl;
+		vkAllocateCommandBuffers(device, &allocInfo, commandBuffers.data());
+		
+		for(int i = 0; i < commandBuffers.size(); i++)
+		{
+			VkCommandBufferBeginInfo beginInfo = {};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			beginInfo.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+			beginInfo.pInheritanceInfo = NULL;
+			
+			cout<<"calling vkBeginCommandBuffer..."<<endl;
+			vkBeginCommandBuffer(commandBuffers[i], &beginInfo);
+			
+			VkRenderPassBeginInfo renderPassInfo = {};
+			renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+			renderPassInfo.renderPass = renderPass;
+			renderPassInfo.framebuffer = swapChainFramebuffers[i];
+			renderPassInfo.renderArea.offset = {0,0};
+			renderPassInfo.renderArea.extent = { 640, 480 };
+			VkClearValue clearColor = {1.0f, 0.0f, 0.0f, 1.0f};
+			renderPassInfo.clearValueCount = 1;
+			renderPassInfo.pClearValues = &clearColor;
+			
+			cout<<"calling vkCmdBeginRenderPass..."<<endl;
+			vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+			
+			vkCmdBindPipeline(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+			
+			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+			
+			vkCmdEndRenderPass(commandBuffers[i]);
+			
+			vkEndCommandBuffer(commandBuffers[i]);
+		}
+		
+		cout<<"done creating Command Buffers!"<<endl<<endl;
+	}
 	void InitVulkan()
 	{
 		cout<<"Initializing Vulkan..."<<endl<<endl;
@@ -641,6 +703,8 @@ class VulkanEngine
 		CreateImageViews();
 		CreateGraphicsPipeline();
 		CreateFramebuffer();
+		CreateCommandPool();
+		CreateCommandBuffers();
 		cout<<"Vulkan Initialized!"<<endl<<endl;
 	}
 	void DestroyVulkan()
